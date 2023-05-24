1a46c1d6f56e76c359e122269669ec9385cbbfbd
Isaias <98565291+isaiasgj0002@users.noreply.github.com>
2023-05-23 19:40:05 -0500

    Agregando efecto parallax en js

--- cf.js
+++ cf.js
@@ -22,3 +22,21 @@ modalTrigger.addEventListener("click", abrirModal);
 
 // Agregar manejador de eventos al botón de cerrar el modal
 modalCloseButton.addEventListener("click", cerrarModal);
+// Obtener referencia a los elementos con la clase .parallax-element
+var parallaxElements = document.querySelectorAll('.parallax-element');
+
+// Función para aplicar el efecto parallax
+function aplicarParallax() {
+  // Obtener el desplazamiento vertical del usuario
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  // Aplicar el desplazamiento a los elementos .parallax-element
+  for (var i = 0; i < parallaxElements.length; i++) {
+    var element = parallaxElements[i];
+    var parallaxOffset = scrollTop * 0.5; // Ajusta la velocidad del parallax aquí
+    element.style.transform = 'translateY(' + parallaxOffset + 'px)';
+  }
+}
+
+// Agregar manejador de eventos al evento scroll
+window.addEventListener('scroll', aplicarParallax);
